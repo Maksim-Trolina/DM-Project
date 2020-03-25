a7db2c028f86c9e40be362e6144d4bc4051aec87
Maksim Panasenko <maks.panasenko01@bk.ru>
2020-03-25 03:22:10 +0300

    The function of multiplying the number by 10 ^ k  is  added

---
+++ Discrete Math Project/MUL_Nk_N.cpp
@@ -0,0 +1,13 @@
+
+int* MUL_Nk_N(int lenNumber, int number[], int power) {
+	
+	int* newNumber = new int[lenNumber + power];
+	for (int i = 0; i < lenNumber;i++) {
+		newNumber[i] = number[i];
+	}
+	for (int i = lenNumber; i < lenNumber + power; i++) {
+		newNumber[i] = 0;
+	}
+	
+	return newNumber;
+}
--- Discrete Math Project/Modules.h
+++ Discrete Math Project/Modules.h
@@ -2,3 +2,5 @@
 int COM_NN_D(int lenNumber1, int lenNumber2, int number1[], int number2[]);
 //Multiplies a number by a digit
 int* MUL_ND_N(int lenNumber, int number[], int digit);
+//Multiplies a positive integer by 10 ^ k
+int* MUL_Nk_N(int lenNumber, int number[], int power);
