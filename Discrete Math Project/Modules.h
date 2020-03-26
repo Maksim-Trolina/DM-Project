c8a13fe347c55fc9c5848d4cd6ed9949985e2e89
ClassicJo <tkachuk-g@list.ru>
2020-03-26 16:57:20 +0300

    Update Modules.h

--- Discrete Math Project/Modules.h
+++ Discrete Math Project/Modules.h
@@ -9,4 +9,5 @@ int* MUL_Nk_N(int lenNumber, int number[], int power,int &lenOutput);
 
 //Add of two numbers
 int* ADD_NN_N(int lenNumber1, int lenNumber2, int &lenOutput, int number1[], int number2[]);//Check for zero
+//Check for zero
 int* NZER_N_B(int number[]);
