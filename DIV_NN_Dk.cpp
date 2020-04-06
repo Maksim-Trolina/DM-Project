ea14d4fc98dbe32e401563e08f7301e5b00ec19f
EugenKoulik <62559389+EugenKoulik@users.noreply.github.com>
2020-04-06 22:10:21 +0300

    Fixed DIV_NN_Dk

--- DIV_NN_Dk.cpp
+++ DIV_NN_Dk.cpp
@@ -5,6 +5,7 @@ int DIV_NN_Dk(int lenNumber1, int lenNumber2, int number1[], int number2[], int&
 	if (!NZER_N_B(number1) || !NZER_N_B(number2)) {
 		return 0;
 	}
+
 	position = 0;
 	int flag = COM_NN_D(lenNumber1, lenNumber2, number1, number2);
 	int digit = 0;
@@ -13,12 +14,21 @@ int DIV_NN_Dk(int lenNumber1, int lenNumber2, int number1[], int number2[], int&
 	int* newNumber1;
 	int lenLocalOutput = 0;
 
+	int* localNumber1 = new int[lenNumber1];
+	int* localNumber2 = new int[lenNumber2];
+
+	for (int i = 0; i < lenNumber1; i++) {
+		localNumber1[i] = number1[i];
+	}
+	for (int i = 0; i < lenNumber2; i++) {
+		localNumber2[i] = number2[i];
+	}
 
 	switch (flag) {
 
 	case 2:
 
-		if (COM_NN_D(lenNumber2, lenNumber2, number1, number2) != 1) {
+		if (COM_NN_D(lenNumber2, lenNumber2, localNumber1, localNumber2) != 1) {
 			position = lenNumber1 - lenNumber2;
 		}
 		else {
@@ -28,13 +38,13 @@ int DIV_NN_Dk(int lenNumber1, int lenNumber2, int number1[], int number2[], int&
 		newNumber2 = MUL_Nk_N(lenNumber2, number2, position, lenLocalOutput);
 		lenNumber2 = lenLocalOutput;
 
-		while (COM_NN_D(lenNumber1, lenNumber2, number1, newNumber2) != 1) {
+		while (COM_NN_D(lenNumber1, lenNumber2, localNumber1, newNumber2) != 1) {
 
 			digit++;
 
-			cup = SUB_NN_N(lenNumber1, lenNumber2, lenLocalOutput, number1, newNumber2);
+			cup = SUB_NN_N(lenNumber1, lenNumber2, lenLocalOutput, localNumber1, newNumber2);
 			for (int i = 0; i < lenLocalOutput; i++) {
-				number1[i] = cup[i];
+				localNumber1[i] = cup[i];
 			}
 			lenNumber1 = lenLocalOutput;
 		}
@@ -43,22 +53,22 @@ int DIV_NN_Dk(int lenNumber1, int lenNumber2, int number1[], int number2[], int&
 
 	case 1:
 
-		if (COM_NN_D(lenNumber1, lenNumber1, number2, number1) != 1) {
+		if (COM_NN_D(lenNumber1, lenNumber1, localNumber2, localNumber1) != 1) {
 			position = lenNumber2 - lenNumber1;
 		}
 		else {
 			position = (lenNumber2 - lenNumber1) - 1;
 		}
 
-		newNumber1 = MUL_Nk_N(lenNumber1, number1, position, lenLocalOutput);
+		newNumber1 = MUL_Nk_N(lenNumber1, localNumber1, position, lenLocalOutput);
 		lenNumber1 = lenLocalOutput;
 
-		while (COM_NN_D(lenNumber2, lenNumber1, number2, newNumber1) != 1) {
+		while (COM_NN_D(lenNumber2, lenNumber1, localNumber2, newNumber1) != 1) {
 
 			digit++;
-			cup = SUB_NN_N(lenNumber2, lenNumber1, lenLocalOutput, number2, newNumber1);
+			cup = SUB_NN_N(lenNumber2, lenNumber1, lenLocalOutput, localNumber2, newNumber1);
 			for (int i = 0; i < lenLocalOutput; i++) {
-				number2[i] = cup[i];
+				localNumber2[i] = cup[i];
 			}
 			lenNumber2 = lenLocalOutput;
 
--- Discrete Math Project.cpp
+++ Discrete Math Project.cpp
@@ -7,27 +7,28 @@ using namespace std;
 int main()
 {
     int* a = new int[4];
-    int* b = new int[3];
+    int* b = new int[4];
     int lenNumber = 3;
-    int len = 0;
     int len1 = 0;
+	int len = 0;
     a[0] = 9;
-    a[1] = 1;
-    a[2] = 0;
+    a[1] = 9;
+    a[2] = 9;
     a[3] = 9;
 
-    b[0] = 2;
-    b[1] = 0;
-    b[2] = 0;
-    
-    int c = DIV_NN_Dk(lenNumber + 1, lenNumber, a, b, len);
-    cout << len << endl;
+    b[0] = 9;
+    b[1] = 9;
+    b[2] = 9;
+	b[3] = 9;
+
+    int c = DIV_NN_Dk(lenNumber + 1, lenNumber + 1, a, b, len);
+	cout << len << endl;
     cout << c << endl;
     for (int i = 0; i < lenNumber + 1; i++) {
         cout << a[i];
     }
     cout << endl;
-    for (int i = 0; i < lenNumber; i++) {
+    for (int i = 0; i < lenNumber + 1; i++) {
         cout << b[i];
     }
     
