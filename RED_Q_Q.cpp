d51e06692d667830fa4879f4f2427e5d5a7d62d7
Maksim Panasenko <maks.panasenko01@bk.ru>
2020-04-15 02:41:38 +0300

    Create empty RED_Q_Q and added method signature

---
+++ RED_Q_Q.cpp
@@ -0,0 +1,8 @@
+#include "Modules.h"
+
+
+void RED_Q_Q(int lenNumerator1, int lenDenominator1, int lenNumerator2, int lenDenominator2, int& lenOutputNumerator, int& lenOutputDenominator,
+	int sign1, int sign2, int& signOutput, int numerator1[], int denominator1[], int numerator2[], int denominator2[], int* numeratorOutput, int* denominatorOutput)
+{
+
+}
